40bbcf09d5b878a7aa0af5b3f0341f3dc7679eea
Yanzen <yanwarsolah@gmail.com>
2022-12-20 15:30:02 +0700

    Tugas ke 27

--- src/app/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -6,6 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-page.component.css']
 })
 export class DashboardPageComponent implements OnInit {
+  daftarFitur: any = [
+    {
+      "title": "Product Management",
+      "image": "https://picsum.photos/200/90?random=1",
+      "link": ""
+    },
+    {
+      "title": "User Management",
+      "image": "https://picsum.photos/200/90?random=2",
+      "link": ""
+    },
+    {
+      "title": "Sales Management",
+      "image": "https://picsum.photos/200/90?random=3",
+      "link": ""
+    },
+    {
+      "title": "Task Management",
+      "image": "https://picsum.photos/200/90?random=4",
+      "link": ""
+    },
+    {
+      "title": "Content Management",
+      "image": "https://picsum.photos/200/90?random=5",
+      "link": ""
+    }
+  ];
+
 
   constructor() { }
 
